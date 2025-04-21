563c9850fa919294886e1a700f331ac843a0d43c
akashmongoosetech <akashraikwar.mongoosetech@gmail.com>
2025-04-21 09:30:27 -0400

    express

--- index.js
+++ index.js
@@ -1,33 +1,18 @@
-const http = require("http");
-const fs = require("fs");
-const url = require ("url");
+const express = require("express");
+const app = express();
 
-const myServer = http.createServer((req, res) => {
-    if (req.url === "/fevicon,ico" ) return res.end();
-    const log = `${Date.now()}: ${req.url} New Req Received\n`;
-
-
-    const myUrl = url.parse(req.url);
-    console.log(myUrl);
-
-    fs.appendFile("log.txt", log, (err, data) => {
-        
-
-        switch (myUrl.pathname) {
-            case '/':
-                res.end("HomePage");
-                break;
-            case '/about':
-                res.end("I am Akash Raikwar");
-                break;
-                case '/about':
-                res.end("I am Akash Raikwar About");
-                break;
-            default:
-                res.statusCode = 404;
-                res.end("404 Not Found");
-        }
-    });
+app.get("/", (req, res) => {
+    res.send("Hello, This is HomePage");
 });
 
-myServer.listen(5000, () => console.log("Server is running"));
+app.get("/about", (req, res) => {
+    res.send("Hello, This is AboutusPage");
+});
+
+app.get("/contact", (req, res) => {
+    res.send("Hello, This is ContactPage");
+});
+
+app.listen(5000, () => {
+    console.log("Server is running on http://localhost:5000");
+});
